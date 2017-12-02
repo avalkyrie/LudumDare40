e1f8df1b1e8140464e1f2d607a1dd3445e230f5b
Avalkyrie <avapierce@gmail.com>
2017-12-01 20:51:29 -0800

    Initial commit with flying started project content

---
+++ CatAirBalloon/Source/CatAirBalloon/CatAirBalloon.cpp
@@ -0,0 +1,8 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "CatAirBalloon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, CatAirBalloon, "CatAirBalloon");
+
+DEFINE_LOG_CATEGORY(LogFlying)
